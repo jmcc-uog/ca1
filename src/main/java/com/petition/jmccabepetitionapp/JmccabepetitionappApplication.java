7770348f1bcc0032277e0aba0004dbd4b93ae505
john <j.mccabe4@universityofgalway.ie>
2023-10-20 18:01:04 +0100

    Adding init files from sping

---
+++ src/main/java/com/petition/jmccabepetitionapp/JmccabepetitionappApplication.java
@@ -0,0 +1,13 @@
+package com.petition.jmccabepetitionapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JmccabepetitionappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JmccabepetitionappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/petition/jmccabepetitionapp/JmccabepetitionappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.petition.jmccabepetitionapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JmccabepetitionappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
